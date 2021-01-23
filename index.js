2ebadf519be02515103132dd62df27aa17a1460c
h_mochizuki <bda5114128@gmail.com>
2021-01-24 05:21:47 +0900

    feat: implements feature of user is typing

--- index.js
+++ index.js
@@ -17,18 +17,23 @@ app.get('/', (req, res) => {
 });
 
 io.on('connection', (socket) => {
-  io.emit('session in', '新しいユーザーが参加しました');
+  io.emit('info', '新しいユーザーが参加しました');
 
   socket.on('chat message', ({ msg, nickname }) => {
     socket.broadcast.emit('chat message', { msg, nickname });
   });
 
+  socket.on('typing', (nickname) => {
+    const name = nickname || 'anonymous';
+    socket.broadcast.emit('info', `${name} is typing...`);
+  });
+
   socket.on('disconnect', () => {
-    io.emit('session out', 'ユーザーが離脱しました');
+    io.emit('info', 'ユーザーが離脱しました');
   });
 
   socket.on('nickname', (nickname) => {
-    socket.emit('nickname', `ニックネームが${nickname}に設定されました`);
+    socket.emit('info', `ニックネームが${nickname}に設定されました`);
   });
 });
 
