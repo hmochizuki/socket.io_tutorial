5ed49486a25076c9b1016f2cf80c9168a5ab96fe
h_mochizuki <bda5114128@gmail.com>
2021-01-24 01:23:51 +0900

    feat: implements chat massage

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const app = require('express')();
-const http = require('http').createServer(app);
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+  res.sendFile(`${__dirname}/index.html`);
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+
+  socket.on('chat message', (msg) => {
+    console.log(`message: ${msg}`);
+  });
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
 });
 
 http.listen(3000, () => {
